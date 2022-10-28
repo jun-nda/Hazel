d530d45222549d8697a30b7d74d69ba8b9daa8ac
jun-nda <junnda@163.com>
2022-10-29 00:25:50 +0800

    dispatch GLFW Event

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,38 +1,48 @@
 #include "hzpch.h"
-
 #include "Application.h"
+
 #include "Hazel/Events/ApplicationEvent.h"
 
 #include <GLFW/glfw3.h>
 
-namespace Hazel{
+namespace Hazel {
 
-	Application::Application( )
-	{
-		m_Window = std::unique_ptr<Window>(Window::Create());
-	}
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
-	Application::~Application( )
-	{
-
-	}
-
-	void Application::Run( )
-	{
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication))
-			while (m_Running)
-			{
-				HZ_TRACE("ASD" );
-				glClearColor(1, 0, 1, 1);
-				glClear(GL_COLOR_BUFFER_BIT);
-				m_Window->OnUpdate();
-			}
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			//HZ_TRACE(e);
-		}
-
-		while (true);
-	}
+Application::Application() {
+    m_Window = std::unique_ptr<Window>(Window::Create());
+    m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 }
+
+Application::~Application() {}
+
+void Application::OnEvent(Event& e) {
+    EventDispatcher dispatcher(e);
+    dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+    HZ_CORE_INFO("{0}", e);
+}
+
+void Application::Run() {
+    WindowResizeEvent e(1280, 720);
+    if (e.IsInCategory(EventCategoryApplication))
+        while (m_Running) {
+            //HZ_TRACE("ASD");
+            glClearColor(1, 0, 1, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
+            m_Window->OnUpdate();
+        }
+    if (e.IsInCategory(EventCategoryInput)) {
+        HZ_TRACE(e);
+    }
+
+    //while (m_Running)
+    //    ;
+}
+
+bool Application::OnWindowClose(WindowCloseEvent& e) {
+    m_Running = false;
+    return true;
+}
+
+} // namespace Hazel
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,19 +1,24 @@
 #pragma once
 #include "Core.h"
+#include "Hazel/Events/ApplicationEvent.h"
 #include "Window.h"
 
-namespace Hazel{
-	class HAZEL_API Application
-	{
-	public:
-		Application( );
-		virtual ~Application( );
-		void Run( );
-	private:
-		std::unique_ptr<Window> m_Window;
-		bool m_Running = true;
-	 };
+namespace Hazel {
+class HAZEL_API Application {
+public:
+    Application();
+    virtual ~Application();
+    void Run();
 
-	// To be defined in CLIENT
-	Application* CreateApplication( );
-}
+    void OnEvent(Event& e);
+
+private:
+    bool OnWindowClose(WindowCloseEvent& e);
+
+    std::unique_ptr<Window> m_Window;
+    bool                    m_Running = true;
+};
+
+// To be defined in CLIENT
+Application* CreateApplication();
+} // namespace Hazel
