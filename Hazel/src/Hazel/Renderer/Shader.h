f489542d1daba51556e6e3d72c9e39f5f356f79a
jun-nda <junnda@163.com>
2022-11-25 15:26:37 +0800

    shader can be read from file

--- Hazel/src/Hazel/Renderer/Shader.h
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -10,7 +10,7 @@ public:
 
     virtual void Bind() const   = 0;
     virtual void Unbind() const = 0;
-
+    static Shader* Create(const std::string& filepath);
     static Shader* Create(const std::string& vertexSrc, const std::string& fragmentSrc);
 };
 } // namespace Hazel
